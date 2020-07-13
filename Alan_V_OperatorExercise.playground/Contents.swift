5bd656a3bc76dc1b18570c5b41e51bc088deaa6c
avillegas-png <67976032+avillegas-png@users.noreply.github.com>
2020-07-13 18:43:45 -0500

    Add files via upload

---
+++ Alan_V_OperatorExercise.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+//Input Values for Claculator. Put your values in the first two variables
+var inputOne = 120
+var inputTwo = 10
+
+//Operations available in the Calculator
+let addition = inputOne + inputTwo
+let subtraction = inputOne - inputTwo
+let multiplication = inputOne * inputTwo
+let division = inputOne / inputTwo
+
+//Print of Answers
+print ("Your variable are \(inputOne) and \(inputTwo)\n")
+print ("Addition Answer: \(addition)\n")
+print ("Subtraction Answer: \(subtraction)\n")
+print ("Multiplication Answer: \(multiplication)\n")
+print ("Division Answer: \(division)\n")
+
